02a38b666140f3980c9bcfde92c539d03c5c7c5a
Joseph Adamson <josephowenadamson@gmail.com>
2023-06-13 12:58:58 +0100

    first release edit

--- Formula/asciify.rb
+++ Formula/asciify.rb
@@ -2,12 +2,13 @@
 
 
 class Asciify < Formula
-    desc "A commandline tool that converts images and gifs into ascii art".
+    desc "A commandline tool that converts images and gifs into ascii art"
     homepage "https://github.com/JosephAdamson/asciify"
     url "https://github.com/JosephAdamson/asciify/releases/download/v0.1.0/asciify-mac.tar.gz"
     sha256 "365eb6fc4191ab17f0be429ff14d8a20ab077f568e76886b6ec0f0f5159a4aed"
     version "0.1.0"
 
+    
     def install
         bin.install "asciify"
     end
