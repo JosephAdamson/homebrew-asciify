826c4c2a514d4b27e7c8c0dcedf6b503cd42c3b7
Joseph Adamson <josephowenadamson@gmail.com>
2023-06-17 14:15:25 +0100

    changed SHA for new binary

--- Formula/asciify.rb
+++ Formula/asciify.rb
@@ -2,7 +2,7 @@ class Asciify < Formula
     desc "Converts images and gifs into ascii art"
     homepage "https://github.com/JosephAdamson/asciify"
     url "https://github.com/JosephAdamson/asciify/releases/download/v0.1.0/asciify-mac.tar.gz"
-    sha256 "365eb6fc4191ab17f0be429ff14d8a20ab077f568e76886b6ec0f0f5159a4aed"
+    sha256 "eef183e63bcb0750d0979a6eedaa38d8ca06f37c2e4625ea99f96a737d9d716b"
     version "0.1.0"
 
     def install
